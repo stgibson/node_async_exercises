6d6cdd9dbff8c64089392be1e1f2406f0e7ac673
Sean Gibson <seanthomasgibson@gmail.com>
2020-12-30 11:28:13 -0800

    Replaced stylesheet, now using image from api. Replaced deck_of_cards/cards.css with deck_of_cards/deck_of_cards.css and updated deck_of_cards/deck_of_cards.html and deck_of_cards/deck_of_cards.js.

--- deck_of_cards/deck_of_cards.js
+++ deck_of_cards/deck_of_cards.js
@@ -22,57 +22,12 @@ axios.get(`${BASE_URL}/new/draw/?count=1`)
 let deckId;
 
 /**
- * Creates an HTML playing card for display
- * @param {string} value The value of the card
- * @param {string} suit The suit of the card
- * @return {string} HTML for playing card
- */
-function generateCard(value, suit) {
-  // get char that represents value
-  switch(value) {
-    case "JACK":
-      value = "j";
-      break;
-    case "QUEEN":
-      value = "q";
-      break;
-    case "KING":
-      value = "k";
-      break;
-    case "ACE":
-      value = "a";
-      break;
-    default:
-      value = value;
-      break;
-  }
-  // get string for representing suit in HTML
-  switch(suit) {
-    case "DIAMONDS":
-      suit = "diams";
-      break;
-    default:
-      suit = suit.toLowerCase();
-      break;
-  }
-
-  return `
-    <div class="card rank-${value} ${suit}">
-      <span class="rank">${value.toUpperCase()}</span>
-      <span class="suit">&${suit};</span>
-    </div>
-  `
-}
-
-/**
  * Draws a card from deck with id deckId
  */
 function drawCard() {
   axios.get(`${BASE_URL}/${deckId}/draw/?count=1`)
     .then(res => {
-      value = res.data.cards[0].value;
-      suit = res.data.cards[0].suit;
-      const $newCard = $(`<li>${generateCard(value, suit)}</li>`);
+      const $newCard = $(`<img src="${res.data.cards[0].image}">`);
       $("#cards").append($newCard);
     })
     .catch(err => console.log(err));
