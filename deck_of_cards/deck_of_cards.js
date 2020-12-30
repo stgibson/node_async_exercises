29caf3f781bfdcc974eeacb35b7af6a1cebf302a
Sean Gibson <seanthomasgibson@gmail.com>
2020-12-29 17:16:31 -0800

    Getting 1 card from newly shuffled deck. Modified deck_of_cards/deck_of_cards.js.

--- deck_of_cards/deck_of_cards.js
+++ deck_of_cards/deck_of_cards.js
@@ -1,1 +1,10 @@
-console.log("Working");
+const BASE_URL = "https://deckofcardsapi.com/api/deck";
+
+// get 1 card from a newly shuffled deck
+axios.get(`${BASE_URL}/new/draw/?count=1`)
+  .then(res => {
+    const value = res.data.cards[0].value;
+    const suit = res.data.cards[0].suit;
+    console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`)
+  })
+  .catch(err => console.log(err));
