f80353e821f5ed03bb3ca88ade0b46e63db1b265
Sean Gibson <seanthomasgibson@gmail.com>
2020-12-29 17:45:31 -0800

    Now draws another card after 1st with same deck. Modified deck_of_cards/deck_of_cards.js.

--- deck_of_cards/deck_of_cards.js
+++ deck_of_cards/deck_of_cards.js
@@ -1,10 +1,20 @@
 const BASE_URL = "https://deckofcardsapi.com/api/deck";
 
-// get 1 card from a newly shuffled deck
+// get 1 card from a newly shuffled deck and get another card from same deck
+let value1;
+let suit1;
 axios.get(`${BASE_URL}/new/draw/?count=1`)
   .then(res => {
-    const value = res.data.cards[0].value;
-    const suit = res.data.cards[0].suit;
-    console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`)
+    value1 = res.data.cards[0].value;
+    suit1 = res.data.cards[0].suit;
+    const deckId = res.data.deck_id;
+    return axios.get(`${BASE_URL}/${deckId}/draw/?count=1`);
+  })
+  .then(res => {
+    const value2 = res.data.cards[0].value;
+    const suit2 = res.data.cards[0].suit;
+    console.log(`${value1.toLowerCase()} of ${suit1.toLowerCase()}`);
+    console.log(`${value2.toLowerCase()} of ${suit2.toLowerCase()}`);
   })
   .catch(err => console.log(err));
+
